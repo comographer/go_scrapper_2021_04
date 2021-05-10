41a872c0a0658708e9d4384c6d3c22537d074c32
Geunmo Kim <kimgeunmo@gmail.com>
2021-05-10 22:26:09 +0200

    const, var, shorthand variable in go

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+const True bool = true
+
 func main() {
-	fmt.Println("Hello World")
+	name := "como"
+	fmt.Println(name)
 }
+
+// fmt is package for formatting
+
+// functions in Go start with uppercase in order to export
+
+// when creating variable, you need to tell type
+
+// when within func, you can create variable like {varaible_name} := {variable}
