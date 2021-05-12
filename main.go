a4fc30a1d5b0d8816a2927bcff29b738efd8469c
Geunmo Kim <kimgeunmo@gmail.com>
2021-05-12 22:48:19 +0200

    struct

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"}
-	for key, value := range nico {
-		fmt.Println(key, value)
-	}
-	fmt.Println(nico)
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name: "nico", age: 18, favFood: favFood}
+	fmt.Println(nico.name)
 }
 
 // fmt is package for formatting
@@ -33,3 +37,5 @@ func main() {
 // switch can be used to avoid too much if else
 
 // adding & shows memory location * see through of memory
+
+// slices, lists, maps, structs
