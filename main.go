0ff1944855ea84a27e09601022bacc743512122c
Geunmo Kim <kimgeunmo@gmail.com>
2021-05-13 20:11:08 +0200

    starting struct practice

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import "fmt"
-
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
-
 func main() {
-	favFood := []string{"kimchi", "ramen"}
-	nico := person{name: "nico", age: 18, favFood: favFood}
-	fmt.Println(nico.name)
+	account := banking.Account{owner: "como", balance: 10000}
 }
 
 // fmt is package for formatting
